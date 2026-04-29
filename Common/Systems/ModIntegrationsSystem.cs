4865c6ff1274b001ba30fd6afb543d98cc343b5e
IronDragon111000 <iron1371904@gmail.com>
2026-04-28 21:55:53 -0700

    Added BossChecklist support
    was susposed to be part of the other commit

---
+++ Common/Systems/ModIntegrationsSystem.cs
@@ -0,0 +1,42 @@
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using System.Collections.Generic;
+using Terraria.ModLoader;
+
+namespace AncientRealms.Common.Systems
+{
+    public class ModIntegrationsSystem : ModSystem
+    {
+        public override void PostSetupContent() {
+			DoBossChecklistIntegration();
+		}
+
+        private void DoBossChecklistIntegration()
+        {
+            if (!ModLoader.TryGetMod("BossChecklist", out Mod bossChecklistMod)) {
+				return;
+			}
+
+            if (bossChecklistMod.Version < new Version(1, 6)) {
+				return;
+			}
+
+            bossChecklistMod.Call(
+				"LogBoss",
+				Mod,
+				"GateKeeper",
+				2.7f,
+				() => BossDownedSystem.downedGateKeeper,
+				ModContent.NPCType<Content.Bosses.GateKeeper.GateKeeper>());
+
+            bossChecklistMod.Call(
+				"LogBoss",
+				Mod,
+				"EldritchVoid",
+				7.8f,
+				() => BossDownedSystem.downedEldritchVoid,
+				ModContent.NPCType<Content.Bosses.EldritchVoid.EldritchVoid>());
+        }
+    }
+}
