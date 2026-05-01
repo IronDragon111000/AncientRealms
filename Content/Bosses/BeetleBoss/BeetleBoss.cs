63dfaf8996fd3e4da2ea16cdc40d9d5785cb330e
IronDragon111000 <iron1371904@gmail.com>
2026-05-01 19:09:26 +0000

    Added files for a future boss

---
+++ Content/Bosses/BeetleBoss/BeetleBoss.cs
@@ -0,0 +1,40 @@
+using Microsoft.Xna.Framework;
+using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent.Bestiary;
+using Terraria.GameContent.ItemDropRules;
+using Terraria.Graphics.CameraModifiers;
+using Terraria.ID;
+using Terraria.Localization;
+using Terraria.ModLoader;
+using System.Linq;
+using static Terraria.ModLoader.ModContent;
+using AncientRealms.Common.Systems;
+
+/*namespace AncientRealms.Content.Bosses.BeetleBoss
+{
+    public sealed partial class BeetleBoss : ModNPC
+    {
+        public override void SetStaticDefaults()
+        {
+            NPCID.Sets.MPAllowedEnemies[NPC.type] = true;
+            NPCID.Sets.BossBestiaryPriority.Add(Type);
+        }
+
+        public override void SetDefaults()
+        {
+            NPC.boss = true;
+            NPC.knockBackResist = 0f;
+            NPC.npcSlots = 10f; 
+            NPC.noGravity = true;
+            NPC.noTileCollide = true;
+            NPC.value = Item.buyPrice(gold: 10);
+            NPC.lifeMax = 6000;
+            NPC.aiStyle = -1;
+            NPC.defense = 15;
+        }
+    }
+}*/
