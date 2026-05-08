15c3f6e8c34731d9a16a9fd7beba6c84eff9e771
IronDragon111000 <iron1371904@gmail.com>
2026-05-08 19:49:43 +0000

    Made TestInvasion have a spawn pool

--- Content/Events/Invasions/InvasionSystem.cs
+++ Content/Events/Invasions/InvasionSystem.cs
@@ -1,10 +1,30 @@
 
 using AncientRealms.Core.Systems;
+using AncientRealms.Content.Events.Invasions.TestInvasion;
 
 namespace AncientRealms.Events.Invasions
 {
-    public class InvasionSystem : ModIntegrationsSystem
+    public class InvasionSystem : ModSystem
     {
-        public static bool[] downedInvasions = {false};
-    }    
+    }
+
+    public class InvasionNPC : ModNPC
+    {
+        public override void EditSpawnPool(IDictionary<int, float> pool, NPCSpawnInfo spawnInfo)
+        {
+             if (TestInvasion.Wave != 0 && spawnInfo.Player.ZoneOverworldHeight && !spawnInfo.Player.ZoneTowerNebula && !spawnInfo.Player.ZoneTowerSolar && !spawnInfo.Player.ZoneTowerStardust && !spawnInfo.Player.ZoneTowerVortex)
+            {
+                pool.Clear();
+                if (Framing.GetTileSafely(spawnInfo.SpawnTileX, spawnInfo.SpawnTileY - 1).WallType is not WallID.DirtUnsafe)
+                {
+                    IDictionary<int, float> spawnpool = TestInvasionNPC.SpawnPool.ElementAt(TestInvasion.Wave);
+                    foreach (KeyValuePair<int, float> key in spawnpool)
+                    {
+                        pool.Add(key.Key, key.Value);
+                    }
+                }
+                return;
+            }
+        }
+    }
 }
